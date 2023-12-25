770da22b03eee42c0da32c1f21fab6634c03d173
Devi Prasad Mishra <89973994+Dmnep58@users.noreply.github.com>
2023-12-25 22:48:31 +0545

    create methods in dom

---
+++ 5.5.0.DOM_CREATE_Method.js
@@ -0,0 +1,26 @@
+
+//1. if we want to add html element with the help of js the we uses --> createElement method in js
+        // like paragraph heading any element.
+
+    var a=  document.createElement("p");
+    console.log(a);
+
+
+
+// 2.  createTextNode is used to write text in the html file
+
+var b =  document.createTextNode("myslef DEvi ");
+console.log(b);
+
+
+// 3. createComment is used to write the comment in the html file.
+
+var c = document.createComment("this is a demo");
+console.log(c);
+
+
+// this will paly the role when we used the append method to add them in the html file.
+
+
+
+
---
+++ 5.5.2.DOm_Append_Methods.js
@@ -0,0 +1,11 @@
+
+// the html file used is  5.5.1.
+
+
+/* The HTML DOM createElement() method is used for creating an HTML element dynamically using JavaScript.
+    It takes the element name as the parameter and creates that element node. You need to use the appendChild() 
+    method to have the newly created element as part of DOM  */
+
+
+    // appends methods can help to attach the create methods inthe html file.
+
