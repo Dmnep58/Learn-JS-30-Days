23be61b93be2d3043ceeb09ae5a8b7946b570936
Devi Prasad Mishra <89973994+Dmnep58@users.noreply.github.com>
2024-01-03 19:21:44 +0545

    ??operator

--- Javascript Programming/9.0.Nullish_Operator.js
+++ Javascript Programming/9.0.Nullish_Operator.js
@@ -1,5 +1,25 @@
 
-//  Nullish coalescing operator ( ?? ) : null undefined
+/*  Nullish coalescing operator ( ?? ) : null undefined */
+
+// the first value will be assigned instead of null and undefined
+
+let val1;
+
+val1 = 5 ?? 10
+
+// console.log(val1)
+
+//if we get null value or undefined value then adjust the code as per the databases cases where there may be a chance of geeting null or undefined
+
+// like 
+
+var val2 = null ?? 10 // here the 10 will be assigned instead of null 
+
+// console.log(val2)
+
+var val3 = undefined ?? 10 // here the 10 will be assigned instead of undefined
+console.log(val3);
+
 
 
 
